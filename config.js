2b096aaefffddb574375fe55cb01f41b0418c69c
RobertLib <rob@hotmail.cz>
2025-05-30 12:32:15 +0200

    Blog

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  site: {
+    title: "RobLib's Blog",
+    description: "Blog about programming and web technologies",
+    author: "Robert Libsansky",
+    baseUrl:
+      process.env.NODE_ENV === "production"
+        ? process.env.BASE_URL || "https://robertlib.github.io/blog"
+        : "http://localhost:3000",
+  },
+  build: {
+    articlesDir: "articles",
+    outputDir: "dist",
+    templatesDir: "templates",
+    staticDir: "static",
+    recentArticlesCount: 10,
+  },
+};
