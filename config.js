2f1367176b7054e1aae25ffea4baf98fc5620bd5
RobertLib <rob@hotmail.cz>
2025-06-10 06:56:24 +0200

    Tests

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: "Robert Libšanský",
     baseUrl:
       process.env.NODE_ENV === "production"
-        ? process.env.BASE_URL || "https://RobertLib.github.io/blog"
+        ? process.env.BASE_URL || "https://robertlib.github.io/blog"
         : "http://localhost:3000",
   },
   build: {
